89fc400d3c3140b7c006a466fe2e329b977ac247
Wajd Alsulaiman <135059431+Wajdals@users.noreply.github.com>
2025-03-22 01:01:57 +0300

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,25 @@
 def players_by_country_and_position(squads_list):
-    pass
+    res = {}
+
+    for player in squads_list:
+        position = player[1]  
+        country = player[6]   
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+
+        if country not in res:
+            res[country] = {}
+        if position not in res[country]:
+            res[country][position] = []
+        res[country][position].append(player_dict)
+
+    return res
