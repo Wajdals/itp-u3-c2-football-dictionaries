08e4484bcbede58df768bd28a4fb47d3df46ed49
Wajd Alsulaiman <135059431+Wajdals@users.noreply.github.com>
2025-03-22 00:37:58 +0300

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,20 @@
 def players_as_dictionaries(squads_list):
-    pass
+    res = []
+    for i in squads_list :
+        di = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+            }
+        res.append(di)
+    return res 
+
+
+
+    
