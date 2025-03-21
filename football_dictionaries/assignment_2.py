26eaf402cc9a641437e110b69b616927e022f890
Wajd Alsulaiman <135059431+Wajdals@users.noreply.github.com>
2025-03-22 01:04:04 +0300

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,23 @@
 def players_by_position(squads_list):
-    pass
+        res = {}  
+
+    for player in squads_list:
+        position = player[1]  
+
+        player_dict = {
+            'number': player[0],
+            'position': player[1],
+            'name': player[2],
+            'date_of_birth': player[3],
+            'caps': player[4],
+            'club': player[5],
+            'country': player[6],
+            'club_country': player[7],
+            'year': player[8],
+        }
+    
+        if position not in res:
+            res[position] = []
+        res[position].append(player_dict)
+
+    return res
